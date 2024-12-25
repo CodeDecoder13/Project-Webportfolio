d10fbc804fbcba93759da51cdeb4b588c5c2d5c8
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-12-25 20:14:50 +0800

    remove redundant words and change picture

--- my-app/src/components/Projects.tsx
+++ my-app/src/components/Projects.tsx
@@ -29,7 +29,7 @@ const Projects = () => {
       title: 'Indoor Air Quality Monitoring and Recommnedation System',
       description: 'a Project created by Collaborating with a team of 4 members for the Capstone Project',
       technologies: ['Flutter', 'FastApi', 'MySql', 'Python','Php'],
-      imageUrl: '/projects/project1.jpg'
+      imageUrl: '/projects/project0.png'
     },
     {
       title: 'iTam_Portal_for_Invitational_Sportfests',
